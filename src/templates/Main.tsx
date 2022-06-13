1b45e46c03eb395cd2481521f8708d40b0718898
Tuan Anh Le <anh.le@myob.com>
2022-06-13 14:33:08 +1000

    feat: remove header

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 
-import { AppConfig } from '@/utils/AppConfig';
-
 type IMainProps = {
   meta: ReactNode;
   children: ReactNode;
@@ -13,43 +11,12 @@ const Main = (props: IMainProps) => (
     {props.meta}
 
     <div className="mx-auto max-w-screen-md">
-      <div className="border-b border-gray-300">
-        <div className="pt-16 pb-8">
-          <div className="text-3xl font-bold text-gray-900">
-            {AppConfig.title}
-          </div>
-          <div className="text-xl">{AppConfig.description}</div>
-        </div>
-        <div>
-          <ul className="flex flex-wrap text-xl">
-            <li className="mr-6">
-              <Link href="/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
-                  Home
-                </a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <Link href="/about/">
-                <a className="border-none text-gray-700 hover:text-gray-900">
-                  About
-                </a>
-              </Link>
-            </li>
-            <li className="mr-6">
-              <a
-                className="border-none text-gray-700 hover:text-gray-900"
-                href="https://github.com/ixartz/Next-js-Boilerplate"
-              >
-                GitHub
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      {/* Navigation */}
 
+      {/* Page Content */}
       <div className="content py-5 text-xl">{props.children}</div>
 
+      {/* Footer */}
       <footer className="footer footer-center mt-5 gap-4 p-2 text-base-content">
         <div>Copyright © 2022 - DaisyUI Collection</div>
         <div className="grid grid-flow-col gap-4">
