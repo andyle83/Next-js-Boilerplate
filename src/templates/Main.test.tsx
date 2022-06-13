b9bebf26379f91d093919c99709fec132a3f37b3
Tuan Anh Le <anh.le@myob.com>
2022-06-13 16:55:08 +1000

    feat: unit test

--- src/templates/Main.test.tsx
+++ src/templates/Main.test.tsx
@@ -1,33 +1,17 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { Main } from './Main';
 
 describe('Main template', () => {
   describe('Render method', () => {
-    it('should have 3 menu items', () => {
+    it('should have menu item', () => {
       render(<Main meta={null}>{null}</Main>);
 
-      const menuItemList = screen.getAllByRole('listitem');
+      const trendingMenu = screen.getByText(/Trending/);
+      const productMenu = screen.getByText(/Products/);
 
-      expect(menuItemList).toHaveLength(3);
-    });
-
-    it('should have a link to support creativedesignsguru.com', () => {
-      render(<Main meta={null}>{null}</Main>);
-
-      const copyrightSection = screen.getByText(/© Copyright/);
-      const copyrightLink = within(copyrightSection).getByRole('link');
-
-      /*
-       * PLEASE READ THIS SECTION
-       * We'll really appreciate if you could have a link to our website
-       * The link doesn't need to appear on every pages, one link on one page is enough.
-       * Thank you for your support it'll mean a lot for us.
-       */
-      expect(copyrightLink).toHaveAttribute(
-        'href',
-        'https://creativedesignsguru.com'
-      );
+      expect(trendingMenu).toBeInTheDocument();
+      expect(productMenu).toBeInTheDocument();
     });
   });
 });
